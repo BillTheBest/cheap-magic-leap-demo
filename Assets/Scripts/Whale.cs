1d7e6afaf6867c80f671f2e9d5f7941cc8b05962
vinjn <vinjn.z@gmail.com>
2016-04-25 22:44:55 +0800

    Add whale and iTween

---
+++ Assets/Scripts/Whale.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Whale : MonoBehaviour {
+
+	Animation anim;
+	List<string> animNames = new List<string>();
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animation>();
+		foreach (AnimationState state in anim)
+		{
+			animNames.Add(state.name);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!anim.isPlaying) {
+			var nextAnimName = animNames[Random.Range(0, animNames.Count - 1)];
+			Debug.LogFormat("play: {0}", nextAnimName);
+			anim.Play(nextAnimName);
+			anim.wrapMode = WrapMode.Once;
+		}
+	}
+}
