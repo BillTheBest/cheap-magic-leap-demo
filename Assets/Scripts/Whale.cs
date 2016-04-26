24a6e808d91ba092ba02c519c20b0ccf52abd4bf
vinjn <vinjn.z@gmail.com>
2016-04-26 17:43:57 +0800

    Disable Whale.cs

--- Assets/Scripts/Whale.cs
+++ Assets/Scripts/Whale.cs
@@ -5,7 +5,10 @@ using System.Collections.Generic;
 public class Whale : MonoBehaviour {
 
 	Animation anim;
+	AnimationState currentState = null;
 	List<string> animNames = new List<string>();
+	
+	float kFadeInSeconds = 0.5f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,15 +17,22 @@ public class Whale : MonoBehaviour {
 		{
 			animNames.Add(state.name);
 		}
+		
+		FadeIn();
+	}
+	
+	void FadeIn() {
+		var nextAnimName = animNames[Random.Range(0, animNames.Count - 1)];
+		Debug.LogFormat("play: {0}", nextAnimName);
+		anim.CrossFade(nextAnimName, kFadeInSeconds);
+		anim.wrapMode = WrapMode.Once;
+		currentState = anim[nextAnimName];
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (!anim.isPlaying) {
-			var nextAnimName = animNames[Random.Range(0, animNames.Count - 1)];
-			Debug.LogFormat("play: {0}", nextAnimName);
-			anim.Play(nextAnimName);
-			anim.wrapMode = WrapMode.Once;
+		if (currentState.time > currentState.length - kFadeInSeconds) {
+			FadeIn();
 		}
 	}
 }
