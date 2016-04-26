a217c00b1aed08c0aa53043d6547380fc7359f44
vinjn <vinjn.z@gmail.com>
2016-04-26 17:25:32 +0800

    Add RenderScale.cs to change stereo rendering scale.

---
+++ Assets/Scripts/RenderScale.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+public class RenderScale : MonoBehaviour {
+
+	[SerializeField] private float m_RenderScale = 1f;              //The render scale. Higher numbers = better quality, but trades performance
+
+	// Use this for initialization
+	void Start () {
+		VRSettings.renderScale = m_RenderScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
